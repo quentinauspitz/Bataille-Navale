fc52d9fd70f598d4914e12f8224ceae5aa3ea2e6
Quentin Auspitz <quentin.auspitz@gmail.com>
2022-10-27 10:19:15 +0200

    projet

---
+++ Bataille_navale_mobile/src/main/java/modele/Joueur_Machine.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modele;
+
+/**
+ *
+ * @author Quentin Auspitz
+ */
+
+public class Joueur_Machine {
+    
+}
