d35da3557518f6778d151cd72b3c32396a27f782
Quentin Auspitz <quentin.auspitz@gmail.com>
2022-11-01 17:23:29 +0100

    Mise à jour

---
+++ Bataille_navale_mobile/src/main/java/modele/plateauModele.java
@@ -0,0 +1,93 @@
+package modele;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author Quentin
+ */
+
+public class plateauModele implements Serializable {
+    private int sizeX;
+    private int sizeY;
+    private LinkedList<Bateau> bateaux;
+
+    public plateauModele(int x, int y) {
+        sizeX = x;
+        sizeY = y;
+        bateaux = new LinkedList<>();
+    }
+
+    /*
+     * Teste si le tir en (x,y) touche et renvoie le type de la touche
+     */
+    public int toucher(int x, int y) {
+        for (Bateau curseur : bateaux)
+            if (curseur.toucher(x, y))
+                if (curseur.couler())
+                    return 2;
+                else
+                    return 1;
+        return 0;
+    }
+
+    /*
+     * Enregistre un nouveau bateau dans sur le modele du plateau
+     */
+    public boolean poser(int x, int y, int direction, int type) {
+        Bateau nouveauBateau = new Bateau(type);
+        nouveauBateau.poser(x, y, direction);
+        for (Bateau curseur : bateaux)
+            if (curseur.intersect(nouveauBateau))
+                return false;
+        bateaux.add(nouveauBateau);
+        return true;
+    }
+
+    /*
+     * Supprime le bateau qui etait sur la case (x,y)
+     */
+    public void supprimerBateau(int x, int y) {
+        Bateau aSupprimer = null;
+        for (Bateau curseur : bateaux)
+            if (curseur.estIci(x, y)) {
+                aSupprimer = curseur;
+                break;
+            }
+        if (aSupprimer != null)
+            bateaux.remove(aSupprimer);
+    }
+
+    public int getSizeX() {
+        return sizeX;
+    }
+
+    public int getSizeY() {
+        return sizeY;
+    }
+
+    public List<Bateau> getListeBateaux() {
+        return bateaux;
+    }
+
+    public void clear() {
+        bateaux.clear();
+    }
+
+    public boolean perdu() {
+        for (Bateau curseur : bateaux)
+            if (curseur.getPv() > 0)
+                return false;
+        return true;
+    }
+
+    public Bateau getBateau(int x, int y) {
+        for (Bateau curseur : bateaux)
+            if (curseur.estIci(x, y))
+                return curseur;
+        return null;
+    }
+
+}
