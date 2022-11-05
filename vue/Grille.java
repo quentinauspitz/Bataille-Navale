dae1300b29ebfe270b15a592ed032a5c6ec7fa18
glogmp <gloriagnompoa@gmail.com>
2022-11-05 22:13:57 +0100

    commit gloria class grille 2

--- vue/Grille.java
+++ vue/Grille.java
@@ -16,3 +16,30 @@ public class Grille {
         }
 
     }
+
+/**
+ * Choix de l'orientation du bateau et de son positionement
+ * Vérifie si le bateau peut être placé
+ * Si oui, génère le bateau
+ */
+
+public boolean posBateau(int[][] plateau, int type, boolean horizontal, int coord_x, int coord_y){
+    boolean allOk=true;
+    int taille=0;
+    int xmax = plateau.length;
+
+    int ymax = plateau[0].length;
+
+    if(type==1){
+        taille=5;
+    }else if (type==2){
+        taille=4;
+    }else if (type==3){
+        taille=3;
+    }else if (type==4){
+        taille=3;
+    }else if (type==5){
+        taille=2;
+    }else{
+        return false;
+    }
